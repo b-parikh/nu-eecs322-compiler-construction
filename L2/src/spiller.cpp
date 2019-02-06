dbf998067a1136dd6ee2751e82215f664a2617fe
ByungjinJun <Byungjin.Jun.55@gmail.com>
2019-02-05 19:20:44 -0600

    spill

--- L2/src/spiller.cpp
+++ L2/src/spiller.cpp
@@ -4,7 +4,13 @@
 namespace L2 {
 
 	void spill(Function f, string spill_var, string spill_str) {
-		//TODO
+        for(Instruction* instruct : f.instructions) {
+            // all write to spill_var -> store to the new stack loc
+            // all read from spill_var -> read from the new stack loc
+            //TODO: consider to use gen/kill set here
+            if(spill_var) {
+            }
+        }
 	}
 
     void spill_wrapper(Program p) {
