f525a4710d0f48a8b55345ac1ef8296fc4e7dda4
ByungjinJun <Byungjin.Jun.55@gmail.com>
2019-02-12 12:55:26 -0600

    neighbor var

--- L2/src/register_allocation.cpp
+++ L2/src/register_allocation.cpp
@@ -6,7 +6,7 @@
 
 namespace L2 {
 
-//    const set_of_str GP_REGISTERS( {"r10", "r11", "r12", "r13", "r14", "r15", "r8", "r9", "rax", "rbp", "rbx", "rcx", "rdi", "rdx", "rsi"} );
+    const set_of_str GP_REGISTERS( {"r10", "r11", "r12", "r13", "r14", "r15", "r8", "r9", "rax", "rbp", "rbx", "rcx", "rdi", "rdx", "rsi"} );
 
     std::vector<REG> ALL_REG {
             REG::r8,
@@ -121,6 +121,7 @@ namespace L2 {
         std::vector<Node> new_IG; // empty IG, start rebuilding
         std::vector<Node> stack = init_stack(fp->IG_nodes);
         std::vector<Node> colored_variables; // can't use set because it'd need custom hash
+		std::vector<REG> color_no_use;
         bool spilled = false;
         //std::cout << "stack: ";
 //        for(auto s : stack)
@@ -148,30 +149,53 @@ namespace L2 {
 //                    std::cout << n << ' ';
 //                }
                 //std::cout << '\n';
-                for(auto r : ALL_REG) {
-                    if(popped.neighbors.find(REG_STR_MAP[r]) != popped.neighbors.end()) {
-						
-                        continue;
-                    }
+				color_no_use.clear();
+				for(auto n : popped.neighbors) {
+					bool is_register = false;
+					for(auto regi : GP_REGISTERS) {
+						if(n == regi) { // skip if the neighbor is a REG
+							is_register = true;
+							break;
+						}
+					}
+					if (is_register)
+						continue;
+//					for(auto r : ALL_REG) {
+//						if(popped.neighbors.find(REG_STR_MAP[r]) != popped.neighbors.end()) {
+//							continue;
+//						}
                     else {
                         //std::cout << "popped var name (reg match found): " << popped.name << '\n';
-                        bool neighbor_has_same_assignment = false;
+                        //bool neighbor_has_same_assignment = false;
                         for(auto& cv : colored_variables) {
-                            if(cv.color == r) {
-                                neighbor_has_same_assignment = true;
-                                break;
+							//std::cout << cv.name << ": "<< REG_STR_MAP[cv.color] << " ; " << REG_STR_MAP[r] << '\n';
+                            if(n == cv.name) { // if the neighbor VAR is in colored_variables
+								for(auto r: ALL_REG) {
+									if (cv.color == r) {
+										color_no_use.push_back(r);
+										break;
+									}
+								}
+								break; // no need to check other values in color_variables
                             }
                         }
+					}
+				}
 
-                        if(neighbor_has_same_assignment)
-                            continue;
-
-                        popped.color = r;
-                        //std::cout << "from the map: " << REG_STR_MAP[popped.color] << '\n';
-                        popped.colored = true;
-                        colored_variables.push_back(popped);
-                        break; // found a register that doesn't interfere; break
+                bool color_found = false;
+				for(auto r : ALL_REG) {
+					for(auto no_r : color_no_use) {
+						if(r != no_r) {
+							color_found = true;
+							popped.color = r;
+							popped.colored = true;
+							//std::cout << "from the map: " << REG_STR_MAP[popped.color] << '\n';
+							colored_variables.push_back(popped);
+							break; // found a register that doesn't interfere; break
+						}
                     }
+					if(color_found)
+						break;
                 }
             }
 //            if(!popped.colored) { // Caller saved register not assigned
@@ -216,7 +240,7 @@ namespace L2 {
                 Item spill_var;
                 //spill_var.labelName = node.first;
                 spill_var.labelName = node_to_spill.name;
-                //std::cout << "var spilled: " << spill_var.labelName << '\n';
+                std::cout << "var spilled: " << spill_var.labelName << '\n';
                 spill_var.type = Type::var;
                 Item spill_str;
                 //spill_str.labelName = node.first;
@@ -254,6 +278,10 @@ namespace L2 {
             for(auto &p : new_F->IG) { // for each pair in the IG
                 Node n;
                 n.name = p.first;
+				std::cout << p.first << "\nneighbors: ";
+				for (auto pk : p.second) 
+					std::cout << pk << ' ';
+				std::cout << '\n';
                 n.neighbors = p.second;
                 new_F->IG_nodes.push_back(n); // IG_nodes is a vector<Node>
             }
